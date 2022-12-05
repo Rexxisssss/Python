efdc64467bfad77152fde99ab83c98e9495d730d
João Afonso Vitorino Reis <103146935+Rexxisssss@users.noreply.github.com>
2022-12-05 22:50:22 +0000

    Add files via upload

---
+++ contas.py
@@ -0,0 +1,12 @@
+a = int( input("Insira o primeiro número: "))
+b = int( input("Insira o segundo número: "))
+
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+
+print ("O valor da soma é ",soma)
+print ("O valor da subtração é ",subtracao)
+print ("O valor da multiplicação é ",multiplicacao)
+print ("O valor da divisão é",divisao)
